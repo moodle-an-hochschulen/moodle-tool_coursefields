a01e66b8851b64e60239bf09c325cf4478293fd0
Alexander Bias <alexander.bias@uni-ulm.de>
2021-01-22 09:33:20 +0100

    Release v3.10-r1

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
  defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_coursefields';
-$plugin->version = 2020092800;
-$plugin->release = 'v3.9-r2';
+$plugin->version = 2021010900;
+$plugin->release = 'v3.10-r1';
 $plugin->requires = 2020110900;
 $plugin->supported = [310, 310];
 $plugin->maturity = MATURITY_STABLE;
